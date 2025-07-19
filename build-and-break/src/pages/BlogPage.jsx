dae086cad3717722c292b7fe2d7614182cb953b3
Ishika Thakur <ishikathakur0708@gmail.com>
2025-07-19 19:48:01 +0530

    trying to deploy frontend

--- build-and-break/src/pages/BlogPage.jsx
+++ build-and-break/src/pages/BlogPage.jsx
@@ -2,6 +2,9 @@ import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
+import './blog.css';
+
 
 function BlogPage() {
     const { id } = useParams();
@@ -11,18 +14,32 @@ function BlogPage() {
         fetch(`https://ishikasblogg-production-efcd.up.railway.app/api/blogs/${id}`)
             .then(res => res.json())
             .then(data =>{
+                console.log(typeof data.content);
                 console.log("Fetched blog:", data); // DEBUG
+                console.log("Blog content:", data.content);
                 setBlog(data)});
     }, [id]);
 
     if (!blog) return <p>Loading...</p>;
-    console
+
 
     return (
-        <article className="prose lg:prose-xl max-w-none text-white dark:text-white">
+        <article className="blog-post-container">
             <h1>{blog.title}</h1>
             <img src={blog.imageUrl} alt={blog.title} className="blog-img" />
-             <ReactMarkdown remarkPlugins={[remarkGfm]}>{blog.content}</ReactMarkdown>
+            const parsedContent = blog.content.replace(/\\n/g, '\n');
+            {blog?.content && (
+              <ReactMarkdown
+                className="blog-markdown"
+                remarkPlugins={[remarkGfm]}
+                rehypePlugins={[rehypeRaw]}
+              >
+                console.log("Snippet:", blog.content.slice(0, 50));
+
+                {blog.content}
+              </ReactMarkdown>
+            )}
+
 
         </article>
     );
